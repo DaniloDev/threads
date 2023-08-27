86391f45b3b3a54d5cf9da94f3763ded6d412768
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-08-27 13:44:54 -0300

    feat: implementation of topbar leftbar rightbar and bottombar and their configs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <>
-      <h1>HOME</h1>
-      <UserButton afterSignOutUrl="/" />
-    </>
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+    </div>
   );
 }
