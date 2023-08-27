e0da5b7a32064ddf98dd069f4dda0fb272705117
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-08-26 21:12:12 -0300

    feat: ajuste de metada layout pasta auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Threads",
   description: "A Next.js 13 Meta Threads Application",
 };
