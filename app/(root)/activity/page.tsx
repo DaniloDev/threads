a89b9ba21ada50ea2999029813b1065ad9621059
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-08-31 18:13:51 -0300

    feat: implementation search

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+  return (
+    <>
+      <h1 className="head-text mb-10">Activity</h1>
+    </>
+  );
+};
+
+export default Page;
---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+  return (
+    <>
+      <h1 className="head-text mb-10">Communities</h1>
+    </>
+  );
+};
+
+export default Page;
