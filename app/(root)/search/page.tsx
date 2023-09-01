fdef4644f95c388280a2dd8645368d7ebd72f0c9
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-09-01 20:38:06 -0300

    feat: searchbar communitybar to search profiles and communities even pagination in both

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,30 +1,39 @@
-import UserCard from "@/components/cards/UserCard";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-async function Page() {
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  // Fetch users
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
+
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
-      {/*  Search Bar*/}
+
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
-          <p className="no-result">No users</p>
+          <p className="no-result">No Result</p>
         ) : (
           <>
             {result.users.map((person) => (
@@ -40,6 +49,12 @@ async function Page() {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
